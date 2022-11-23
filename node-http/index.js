99ffb2f5a4b6fe876bab2f43cd44127bbe725a8a
Akash Panwar <akashpanwar9976@gmail.com>
2022-11-23 21:41:52 +0530

    working with postman

--- node-http/index.js
+++ node-http/index.js
@@ -1,13 +1,70 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log('Server created');
+  console.log(`Request for ${req.url} by method ${req.method}`);
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>Hello world!</h1></body></html>');
+  if (req.method == 'GET') {
+    let fileUrl;
+    if (req.url == '/') fileUrl = '/index.html';
+    else fileUrl = req.url;
+
+    let filePath = path.resolve(`./public${fileUrl}`);
+    let fileExt = path.extname(filePath);
+
+    if (fileExt == '.html') {
+      if (!fs.existsSync(filePath)) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(
+          `<html>
+            <body>
+              <h1>Error 404 ${fileUrl} not found</h1>
+            </body>
+          </html>`
+        );
+        return;
+      }
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      fs.createReadStream(filePath).pipe(res);
+
+      //   fs.exists(filePath, (exists) => {
+      //     if (!exists) {
+      //       res.statusCode = 404;
+      //       res.setHeader('Content-Type', 'text/html');
+      //       res.end(
+      //         `<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`
+      //       );
+
+      //       return;
+      //     }
+
+      // res.end(`<>`);
+      //   });
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(
+        `<html><body><h1>Error 404: ${fileUrl} not an HTML file</h1></body></html>`
+      );
+
+      return;
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(
+      `<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`
+    );
+
+    return;
+  }
 });
 
 server.listen(port, hostname, () => {
